c632c0112390fff4e5c0ac8a6916b3632ad728d2
Raza Muhammad <waistband161@gmail.com>
2024-01-24 20:50:20 +0500

    dom practice 6

--- dom/script.js
+++ dom/script.js
@@ -1,13 +1,13 @@
-document.addEventListener("DOMContentLoaded");function(){
+document.addEventListener("DOMContentLoaded",function(){
     const list = document.querySelector('.movie-list ul');
-    const forms = document.forms
+    const forms = document.forms;
 
 
     //delete movie
     list.addEventListener('click',(e)=>{
         if(e.target.className=='delete'){
-            const li = e.target.parentElement
-            li.parentNode.removeChild(li)
+            const li = e.target.parentElement;
+            li.parentNode.removeChild(li);
         }
-    }
-}
+    });
+})
